ceb8e4b0b05f449550e697a79c4063d15e4fe999
benoithusson <benoit.husson@liip.ch>
2023-01-24 10:20:26 +0100

    LIPPMANN-5: Add second page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ function Home() {
   return (
     <div className={styles.Pagecontainer}>
       <CustomLink label="A propos" link="/a-propos" />
+      <CustomLink label="Gabriel Lippmann" link="/gabriel-lippmann" />
     </div>
   )
 }

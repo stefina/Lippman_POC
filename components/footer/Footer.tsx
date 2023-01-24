97f653a45e154c68d4e49633827337c5378ae874
benoithusson <benoit.husson@liip.ch>
2023-01-24 12:45:12 +0100

    LIPPMANN-30: Finish implement footer on mobile

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import styles from './Footer.module.scss'
+import CustomLink from '../../components/link/Link'
 
 export default function Footer() {
-  return <div>FOOTER</div>
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li className={styles.navItem}>
+          <CustomLink label="Á propos" link="/a-propos" />
+        </li>
+        <li className={styles.navItem}>
+          <CustomLink label="Gabriel Lippmann" link="/gabriel-lippmann" />
+        </li>
+      </ul>
+    </nav>
+  )
 }

7c367145417579a67f34646f55fa656e0c763cbf
benoithusson <benoit.husson@liip.ch>
2023-01-26 05:52:59 +0100

    LIPPMANN-32: Start to implement header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,19 +1,54 @@
 import React from 'react'
+import CustomLink from '../link/Link'
 import styles from './Header.module.scss'
 
 export default function Header() {
   return (
-    <div className={styles.headerContainer}>
-      <div className={styles.logoAndSearchBarContainer}>
-        <div className={styles.logo}>Bloc Elysee</div>
-        <div className={styles.searchBar}>
-          <input type="search" id="search-bar" name="search-bar" />
+    <>
+      <div className={styles.headerContainer}>
+        {/* LOGO & SEARCH */}
+        <div className={styles.logoAndSearchBarContainer}>
+          <div className={styles.logo}>
+            <img src="./icons/logo.svg" width="" alt="" />
+          </div>
+
+          <div className={styles.searchBarContainer}>
+            <div className={styles.searchBar}>
+              <label for="search-bar" className={styles.label}>
+                SEARCH:
+              </label>
+              <input type="search" id="search-bar" name="search-bar" />
+            </div>
+          </div>
+        </div>
+        {/* NAV */}
+        <div className={styles.navContainer}>
+          {/* <div className={styles.nav}> */}
+          <nav>
+            <span className={styles.viz}>VIZ: </span>
+            <ul>
+              <li>
+                <CustomLink
+                  label="Creation Timeline"
+                  link="/creation-timeline"
+                />
+              </li>
+              <li>
+                <CustomLink
+                  label="Visual Content Map"
+                  link="/visual-content-mag"
+                />
+              </li>
+              <li>
+                <CustomLink
+                  label="Artwork Location Map"
+                  link="/artwork-location-map"
+                />
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
-
-      <nav className={styles.nav}>
-        <div>Nav</div>
-      </nav>
-    </div>
+    </>
   )
 }

178f40712158a8dbf756dcc9a0271331cce49b90
Marin Aeschbach <marin.aeschbach@gmail.com>
2023-02-03 09:57:15 +0100

    LIPPMANN-32: Fix import

--- pages/creation-timeline/index.tsx
+++ pages/creation-timeline/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../../styles/pages/CreationTimeline.module.scss';
+import styles from '../../styles/pages/CreationTimeLine.module.scss';
 
 export default function CreationTimeline() {
   return <div className={styles.creationTimeLine}>Creation Timeline</div>;

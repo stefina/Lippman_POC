dfafd748d1c7e7ca78c009173241d29f8c4a2aad
Stefanie Taepke <stefanie.taepke@liip.ch>
2023-02-16 14:12:07 +0100

    fix: fix imports

--- pages/a-propos/index.tsx
+++ pages/a-propos/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Text from '../../components/text/Text';
-import CustomLink from '../../components/link/Link';
-import Heading from '../../components/heading/Heading';
+import { Text } from '../../components/Text/Text';
+import Heading from '../../components/Heading/Heading';
 import { CustomLink } from '../../components/link/Link';
 
 export default function AProposPage() {

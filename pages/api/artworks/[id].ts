178b9e5010bfda011ce104ccc44f059e93783031
Stefanie Taepke <stefanie.taepke@liip.ch>
2023-02-16 09:27:35 +0100

    style: add pretty code-style

--- pages/api/artworks/[id].ts
+++ pages/api/artworks/[id].ts
@@ -1,18 +1,18 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { artworks } from '../../../data'
-import type { Artwork, ResponseError } from '../../../interfaces/artwork'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { artworks } from '../../../data';
+import type { Artwork, ResponseError } from '../../../interfaces/artwork';
 
 export default function artworkHandler(
   req: NextApiRequest,
   res: NextApiResponse<Artwork | ResponseError>
 ) {
-  const { query } = req
-  const { id } = query
-//   const artwork = artworks.find((a) => a.id === id)
-const artwork = artworks.find((a) => a.id === id)
+  const { query } = req;
+  const { id } = query;
+  //   const artwork = artworks.find((a) => a.id === id)
+  const artwork = artworks.find((a) => a.id === id);
 
   // artwork with id exists
   return artwork
     ? res.status(200).json(artwork)
-    : res.status(404).json({ message: `Artwork with id: ${id} not found.` })
+    : res.status(404).json({ message: `Artwork with id: ${id} not found.` });
 }

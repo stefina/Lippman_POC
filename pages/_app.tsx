61f795a4b208f4bf68274be3651f35549446fff5
Marin Aeschbach <marin.aeschbach@gmail.com>
2023-02-17 10:57:43 +0100

    Update next.js and re-add custom font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,78 +1,81 @@
 import '../styles/main.scss';
 import type { AppProps } from 'next/app';
 import DefaultLayout from '../components/DefaultLayout';
-// import localFont from '@next/font/local';
+import localFont from '@next/font/local';
 import { themeClass } from '../styles/theme.css';
 import '../styles/global.css';
+import { composeClassNames } from '../utils/composeClassNames';
 
-// const photoElyseeFont = localFont({
-//   src: [
-//     {
-//       path: './fonts/PhotoElysee-Bold.woff',
-//       weight: '900',
-//       style: 'normal',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-Bold.woff2',
-//       weight: '900',
-//       style: 'normal',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-BoldItalic.woff',
-//       weight: '900',
-//       style: 'italic',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-BoldItalic.woff2',
-//       weight: '900',
-//       style: 'italic',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-DisplayBold.woff',
-//       weight: '900',
-//       style: 'normal',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-DisplayBold.woff2',
-//       weight: '900',
-//       style: 'normal',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-DisplayBoldItalic.woff',
-//       weight: '900',
-//       style: 'italic',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-DisplayBoldItalic.woff2',
-//       weight: '900',
-//       style: 'italic',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-Italic.woff',
-//       weight: '400',
-//       style: 'italic',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-Italic.woff2',
-//       weight: '400',
-//       style: 'italic',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-Regular.woff2',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: './fonts/PhotoElysee-Regular.woff',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//   ],
-// });
+const photoElyseeFont = localFont({
+  src: [
+    {
+      path: './fonts/PhotoElysee-Bold.woff',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PhotoElysee-Bold.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PhotoElysee-BoldItalic.woff',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: './fonts/PhotoElysee-BoldItalic.woff2',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: './fonts/PhotoElysee-DisplayBold.woff',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PhotoElysee-DisplayBold.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PhotoElysee-DisplayBoldItalic.woff',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: './fonts/PhotoElysee-DisplayBoldItalic.woff2',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: './fonts/PhotoElysee-Italic.woff',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/PhotoElysee-Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/PhotoElysee-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PhotoElysee-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DefaultLayout className={[themeClass].join(' ')}>
+    <DefaultLayout
+      className={composeClassNames(themeClass, photoElyseeFont.className)}
+    >
       <Component {...pageProps} />
     </DefaultLayout>
   );

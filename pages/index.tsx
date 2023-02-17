20f986b1eb4dcd8f6a640341c9c3c66bf1b68d4c
Marin Aeschbach <marin.aeschbach@gmail.com>
2023-02-17 10:40:17 +0100

    Add up to date index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,33 @@
-import styles from '../styles/Home.module.scss';
-import { CustomLink } from '../components/link/Link';
+import Head from 'next/head';
+import { Artwork, ArtworkCard } from '../components/ArtworkCard';
+import { Box } from '../components/Box';
+import { Grid } from '../components/Grid';
+import testPicture from './lippmann-default.jpg';
 
-function Home() {
+const artwork: Artwork = {
+  author: 'Gabriel Lippmann',
+  image: testPicture,
+  title: 'Haus am See',
+  year: '1904',
+  id: 'lippmann',
+};
+
+const mockContent = Array.from({ length: 10 }, (_, i) => ({
+  ...artwork,
+  id: `${artwork.id}${i}`,
+}));
+
+export default function Home() {
   return (
-    <div className={styles.Pagecontainer}>
-      <div>
-        <CustomLink href="/a-propos">A propos</CustomLink>
-      </div>
-      <div>
-        <CustomLink href="/gabriel-lippmann">Gabriel Lippmann</CustomLink>
-      </div>
-    </div>
+    <Box as="main" marginTop={4} marginBottom={6}>
+      <Head>
+        <title>Gabriel Lippmann | Catalogue Raisonnée</title>
+      </Head>
+      <Grid>
+        {mockContent.map((artwork) => (
+          <ArtworkCard key={artwork.id} {...artwork} />
+        ))}
+      </Grid>
+    </Box>
   );
 }
-
-export default Home;

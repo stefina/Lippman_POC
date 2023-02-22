b3493041b1758e7dfb38135202f7396773dc0bc0
Stefanie Taepke <stefanie.taepke@liip.ch>
2023-02-22 15:19:36 +0100

    refactor: get value directly from map and fallback

--- pages/artwork/fetch-artworks.tsx
+++ pages/artwork/fetch-artworks.tsx
@@ -42,8 +42,21 @@ async function getObjectValue(dataset: any): Promise<string> {
   var value = '';
   for (const datasetQuad of dataset) {
     value = `${datasetQuad.obj.value}`;
+function getObjectValue(dataset: any) {
+  // A dataset looks like this:
+  // Map(1) {
+  //   '1:1:1:' => {
+  //     obj: Literal {
+  //       value: 'Plaque Lippmann: Mont Cervin',
+  //       datatype: [NamedNode],
+  //       language: ''
+  //     }
+  //   }
+  // }
+  if (dataset._quads.entries().next().value && dataset._quads.entries().next().value.length) {
+    return dataset._quads.entries().next().value[1].obj.value;
   }
-  return value;
+  return "undefined";
 }
 
 async function getValue(stream: any) {

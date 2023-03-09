03bfda5eb40f4a9edd60490fdd2ed4b68b06d415
Marin Aeschbach <marin.aeschbach@gmail.com>
2023-03-09 15:20:17 +0100

    Add real data to starting page, clean up

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -1,40 +1,12 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
 import { useContext, useEffect } from 'react';
-
 import { Artwork, ArtworkCard } from '../../components/ArtworkCard';
-import { ArtworkDetailCloseButton } from '../../components/ArtworkDetailCloseButton/ArtworkDetailCloseButton';
-import { ArtworkDetailCluster } from '../../components/ArtworkDetailCluster';
 import { Box } from '../../components/Box';
-import { Button } from '../../components/Button';
 import { Grid } from '../../components/Grid';
-import { Heading } from '../../components/Heading';
-import { IconArrowRight } from '../../components/Icons/IconArrowRight';
-import { Overlay } from '../../components/Overlay';
-import { Stack } from '../../components/Stack';
-import { Text } from '../../components/Text';
-
-import testPicture from '../lippmann-default.jpg';
-import testPicture0 from '../lippmann.jpg';
-import testPicture1 from '../lippmann2.jpg';
-import testPicture2 from '../lippmann3.jpg';
-import testPicture3 from '../lippmann4.jpg';
-import testPicture4 from '../lippmann5.jpg';
-import testPicture5 from '../lippmann6.jpg';
-import testPicture6 from '../lippmann7.jpg';
-import testPicture7 from '../lippmann8.jpg';
-import { ActionType, AppContext } from '../_app';
-import {
-  imageStyle,
-  imageWrapperStyle,
-} from '../../components/ArtworkDetail/ArtworkDetail.css';
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
 import { ArtworkDetail } from '../../components/ArtworkDetail';
-import { client } from '../../utils/client';
-import { isTruthy } from '../../utils/isTruthy';
-import { mapArtwork } from '../../utils/mapArtwork';
-import { GetStaticPaths, GetStaticProps } from 'next';
 import { getArtworks } from '../../utils/getArtworks';
+import { ActionType, AppContext } from '../_app';
 
 interface ArtworkDetailPageProps {
   artworks: Artwork[];
@@ -50,11 +22,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps = async ({
+export const getStaticProps: GetStaticProps<ArtworkDetailPageProps> = async ({
   params,
-}): Promise<{
-  props: ArtworkDetailPageProps;
-}> => {
+}) => {
   const artworks = await getArtworks();
   const currentArtwork = artworks.find((artwork) =>
     params ? artwork.id === params.id : false
@@ -71,7 +41,7 @@ export const getStaticProps: GetStaticProps = async ({
 export default function ArtworkDetailPage({
   artworks,
   currentArtwork,
-}: ArtworkDetailPageProps) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const { dispatch } = useContext(AppContext);
 
   useEffect(() => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,34 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import { Artwork, ArtworkCard } from '../components/ArtworkCard';
 import { Box } from '../components/Box';
 import { Grid } from '../components/Grid';
-import testPicture from './lippmann-default.jpg';
-import testPicture0 from './lippmann.jpg';
-import testPicture1 from './lippmann2.jpg';
-import testPicture2 from './lippmann3.jpg';
-import testPicture3 from './lippmann4.jpg';
-import testPicture4 from './lippmann5.jpg';
-import testPicture5 from './lippmann6.jpg';
-import testPicture6 from './lippmann7.jpg';
-import testPicture7 from './lippmann8.jpg';
+import { getArtworks } from '../utils/getArtworks';
 
-const artwork: Artwork = {
-  author: 'Gabriel Lippmann',
-  image: testPicture,
-  title: 'Haus am See',
-  year: '1904',
-  owner: 'Plateforme 10',
-  id: 'lippmann',
+interface HomePageProps {
+  artworks: Artwork[];
+}
+
+export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+  const artworks = await getArtworks();
+
+  return {
+    props: {
+      artworks,
+    },
+  };
 };
 
-const pictures = [
-  testPicture0,
-  testPicture1,
-  testPicture2,
-  testPicture3,
-  testPicture4,
-  testPicture5,
-  testPicture6,
-  testPicture7,
-];
-
-const mockContent = Array.from({ length: 8 }, (_, i) => ({
-  ...artwork,
-  id: `${artwork.id}${i}`,
-  image: pictures[i],
-  title: `${artwork.title} ${i + 1}`,
-}));
-
-export default function Home() {
+export default function Home({
+  artworks,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Box as="main" flexGrow="1" marginTop={4} marginBottom={6}>
       <Head>
         <title>Gabriel Lippmann | Catalogue Raisonnée</title>
       </Head>
       <Grid>
-        {mockContent.map((artwork) => (
+        {artworks.map((artwork) => (
           <ArtworkCard key={artwork.id} {...artwork} />
         ))}
       </Grid>

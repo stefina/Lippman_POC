343cc769ad41b4ba137f849abd0f0eca9e0310b2
Stefanie Taepke <stefanie.taepke@liip.ch>
2023-03-17 09:58:56 +0100

    feat: add header to Getty-Client to identify us

--- utils/gettyClient.ts
+++ utils/gettyClient.ts
@@ -1,5 +1,9 @@
 import ParsingClient from 'sparql-http-client/ParsingClient';
 
 export const gettyClient = new ParsingClient({
-  endpointUrl: 'https://data.getty.edu/vocab/sparql',
+  endpointUrl: 'https://data.getty.edu/museum/collection/sparql',
+  headers: {
+    'user-agent':
+      'Liip/Lippmann/POC/0.0 (https://lippmann.elysee.ch/, opendata@liip.ch',
+  },
 });

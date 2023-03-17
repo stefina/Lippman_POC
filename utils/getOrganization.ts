6fcad023be628fa9af591b0ccd4ecec94b591b30
Stefanie Taepke <stefanie.taepke@liip.ch>
2023-03-17 12:44:29 +0100

    refactor: use string function instead of query
    we don't get the information directly from the lippmann-graph

--- utils/getOrganization.ts
+++ utils/getOrganization.ts
@@ -22,20 +22,10 @@ export async function getOrganizationURL(link: string) {
 }
 
 export async function getWikiDataOrganizationURL(link: string) {
-  const wikiDataOrganizationURLStream = await lippmannClient.query.select(`
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
-    PREFIX fn: <http://www.w3.org/2005/xpath-functions#>
-    SELECT DISTINCT * WHERE {
-        SERVICE <https://api.triplydb.com/datasets/FredericNoyer/lippmann/services/lippmann/sparql> {
-        GRAPH <https://triplydb.com/FredericNoyer/lippmann/graphs/default> {
-            <${link}/Type> <http://www.cidoc-crm.org/cidoc-crm/P2_has_type> ?obj .
-        }
-        }
-    } 
-  `);
-  return getValueFromResultRows(wikiDataOrganizationURLStream);
+  const organizationURL = await getOrganizationURL(link);
+  const organizationID = await getOrganizationID(organizationURL);
+
+  return 'https://www.wikidata.org/wiki/' + organizationID;
 }
 
 export async function getOrganizationName(link: string) {

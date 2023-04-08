4c4402a01ae94fd7850c6339be5d5c4e9c2ba905
Stefanie Taepke <stefanie.taepke@liip.ch>
2023-04-08 13:26:19 +0200

    fix: removing breaking statements
    Virtuoso 37000 Error SP031

--- utils/searchArtworks.ts
+++ utils/searchArtworks.ts
@@ -2,6 +2,7 @@ import { lippmannClient } from './lippmannClient';
 import { isTruthy } from './isTruthy';
 import { mapArtwork } from './mapArtwork';
 
+// TODO: test this!
 export async function searchArtworks(term: string) {
   const dataset = await lippmannClient.query.select(`
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -9,14 +10,10 @@ export async function searchArtworks(term: string) {
     PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
     PREFIX fn: <http://www.w3.org/2005/xpath-functions#>
     SELECT * WHERE {
-    SERVICE <https://api.triplydb.com/datasets/FredericNoyer/lippmann/services/lippmann/sparql> {
-      GRAPH <https://triplydb.com/FredericNoyer/lippmann/graphs/default> {
-          ?subj rdfs:label ?obj .
-          FILTER regex(?obj, "${term}", "i")
-          FILTER regex(?subj, "^https://pe.plateforme10.ch/Artwork/[0-9]{5}/[0-9]{9}$", "")
-      }
+      ?subj rdfs:label ?obj .
+      FILTER regex(?obj, "${term}", "i")
+      FILTER regex(?subj, "^https://pe.plateforme10.ch/Artwork/[0-9]{5}/[0-9]{9}$", "")
     }
-  }
  `);
 
   const artworksOrUndef = await Promise.all(dataset.map(mapArtwork));

5683341db8300d59f02d0da8a4e3629a00e17594
Marin Aeschbach <marin.aeschbach@gmail.com>
2023-05-09 13:58:21 +0200

    Fix owner on artwork tile

--- utils/getHasCurrentOwner.ts
+++ utils/getHasCurrentOwner.ts
@@ -7,7 +7,7 @@ export async function getHasCurrentOwner(link: string) {
     ${prefixes}
     
     SELECT DISTINCT ?obj WHERE {
-        <https://pe.plateforme10.ch/Artwork/15053/000000005> cidoc:P52_has_current_owner ?hasCurrentOwner .
+        <${link}> cidoc:P52_has_current_owner ?hasCurrentOwner .
   		?hasCurrentOwner rdfs:label ?obj .
     }
   `);
